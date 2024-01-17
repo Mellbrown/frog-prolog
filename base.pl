5795ba131905e416ec806b9fa2d38c26a1781833
mellbrown <2mellbrown@gmail.com>
2024-01-17 16:52:37 +0900

    good

--- base.pl
+++ base.pl
@@ -1,16 +1,29 @@
-:-op(900, fx, query).
-:-op(900, fx, class).
-:-op(900, fx, fact).
-  
-query(Query):-
-  fact(Fact),
-  member(Query, Fact).
+:- multifile [(class)/1, (fact)/1, (implict)/1].
 
-:- discontiguous('/'(class ,1)).
+:- op(900, fx, class).
+:- op(900, fx, fact).
+:- op(900, fx, explict).
+:- op(900, fx, implict).
+:- op(900, fx, query).
+
 class _ :- false.
-
-:- discontiguous('/'(fact, 1)).
 fact [].
+
+explict X :- fact(Z), member(X, Z).
+implict X :- explict X.
+
+:- dynamic result_list/1.
+result_list([]).
+
+query Query :- 
+  implict Query,
+  result_list(OldList),
+  \+ member(Query, OldList),
+  retract(result_list(OldList)),
+  assertz(result_list([Query | OldList]));
+  retract(result_list(_)),
+  assertz(result_list([])).
+  
 
 checking([]) :- true.
 checking([ Inst | Fact ]) :-
@@ -27,5 +40,3 @@ frog_check :-
   false;
   fact(Fact), 
   checking(Fact).
-
-
--- date.pl
+++ date.pl
@@ -5,10 +5,17 @@ class date(Y, M, D):-
   integer(M),
   integer(D).
 
+class date(Y, M):-
+  integer(Y),
+  integer(M).
+
+implict date(Y, M) :- implict date(Y, M, _).
+implict date(Y) :- implict date(Y, _).
+
 fact [
-  date(2024, 1, 1),
-  date(2024, 1, 2),
-  date(2024, 1, 3)
+  date(2024, 2),
+  date(2024, 1)
 ].
+
 
 :- initialization(frog_check).
