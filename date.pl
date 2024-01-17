1162b5c80b6eadaefc9339c4f3e56a801d5c7d1f
mellbrown <2mellbrown@gmail.com>
2024-01-17 16:57:56 +0900

    test

--- date.pl
+++ date.pl
@@ -9,12 +9,24 @@ class date(Y, M):-
   integer(Y),
   integer(M).
 
+class date(Y):-
+  integer(Y).
+
+class exact_date(Y, M, D):-
+  integer(Y),
+  integer(M),
+  integer(D).
+
 implict date(Y, M) :- implict date(Y, M, _).
 implict date(Y) :- implict date(Y, _).
+implict exact_date(Y, M, D) :- implict date(Y, M, D).
+implict exact_date(Y, M, 1) :- implict date(Y, M).
+implict exact_date(Y, 1, 1) :- implict date(Y).
 
 fact [
   date(2024, 2),
-  date(2024, 1)
+  date(2024, 1),
+  exact_date(2024, 1, 1)
 ].
 
 
