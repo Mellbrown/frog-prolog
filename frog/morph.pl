ffac845bfa3823f119c74678ce65b2e33aee5937
mellbrown <2mellbrown@gmail.com>
2024-01-22 11:44:50 +0900

    fix

--- frog/morph.pl
+++ frog/morph.pl
@@ -3,22 +3,36 @@
 :- multifile (morph)/1.
 :- table (morph)/1.
 :- table (~>)/2.
-:- table (<~)/2.
-:- table (<~>)/2.
 
-morph A ~> A.
-morph A <~ A.
-morph A <~> A.
 
-morph A ~> B :- morph B <~ A.
-morph A <~ B :- morph A ~> B.
+morph V1 ~> V2 :- var(V1), var(V2), V1 = V2, !.
 
-morph A ~> B :- morph A <~> B.
-morph A ~> B :- morph B <~> A.
+morph A ~> A :- nonvar(A), \+ is_list(A), \+ compound(A).
 
-morph A <~ B :- morph A <~> B.
-morph A <~ B :- morph B <~> A.
+morph A ~> C :-
+  nonvar(A),
+  morph A ~> B, A \= B,
+  morph B ~> C, B \= C,
+  A \= C;
+  nonvar(C),
+  morph B ~> C, B \= C,
+  morph A ~> B, A \= B,
+  A \= C.
 
-morph A <~> B :- morph B <~> A.
+morph [] ~> [].
+morph [A] ~> [B] :- 
+  morph A ~> B.
+morph [A | As] ~> [B | Bs] :-
+  morph A ~> B,
+  morph As ~> Bs.
 
-morph A ~> C:- morph A ~> B, morph B ~> C.
+morph A ~> B :-
+  nonvar(A), compound(A), \+ is_list(A), \+ atomic(B),
+  compound_name_arguments(A, N, As),
+  morph As ~> Bs,
+  compound_name_arguments(B, N, Bs);
+  nonvar(B), compound(B), \+ is_list(B), \+ atomic(A),
+  compound_name_arguments(B, N, Bs),
+  morph As ~> Bs,
+  compound_name_arguments(A, N, As).
+
--- frog/typeof/typeof_compound.pl
+++ frog/typeof/typeof_compound.pl
@@ -1,8 +1,7 @@
 :- use_module('__typeof').
 
 O typeof T :-
-  (compound(O); var(O)),
-  compound_name_arguments(O, T0, Os),
+  (compound(O); var(O)), compound_name_arguments(O, T0, Os),
   full:(Os typeof Ts),
   (
     (compound(T); var(T)), compound_name_arguments(T, T0, Ts);
